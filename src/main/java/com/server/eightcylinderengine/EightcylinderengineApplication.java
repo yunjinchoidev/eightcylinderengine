0d86f31b73ad0331556dbcb56030e511b1f24290
yunjinchoidev <yunjinchoidev@gmail.com>
2022-08-02 21:56:24 +0900

    프로젝트 최초 커밋

---
+++ src/main/java/com/server/eightcylinderengine/EightcylinderengineApplication.java
@@ -0,0 +1,13 @@
+package com.server.eightcylinderengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightcylinderengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EightcylinderengineApplication.class, args);
+    }
+
+}
